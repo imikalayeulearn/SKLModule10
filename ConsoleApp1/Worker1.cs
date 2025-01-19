35c6bab8887c0ab9771e12e168d2e0b83e868ebc
imikalayeulearn <i.n@relsoftgroup.com>
2025-01-19 12:22:53 +0200

    Commit 1 podcast

---
+++ ConsoleApp1/Worker1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public interface IWorker
+    {
+        void Work();
+    }
+    internal class Worker1 : IWorker
+    {
+        ILogger Logger { get; }
+
+        public Worker1(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker1 начал работу");
+            Thread.Sleep(1000);
+            Logger.Event("Worker 1 закончил работу");
+        }
+    }
+    internal class Worker2 : IWorker
+    {
+        ILogger Logger { get; }
+
+        public Worker2(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker2 начал работу");
+            Thread.Sleep(1000);
+            Logger.Event("Worker 2 закончил работу");
+        }
+    }
+    internal class Worker3 : IWorker
+    {
+        ILogger Logger { get; }
+
+        public Worker3(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker3 начал работу");
+            Thread.Sleep(1000);
+            Logger.Event("Worker 3 закончил работу");
+        }
+    }
+}
